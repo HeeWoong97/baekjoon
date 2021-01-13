f76484723043b1d5d5735df5ebae013fae711147
HeeWoong97 <ungung97@DESKTOP-HG6HCNO.localdomain>
2021-01-13 21:56:21 +0900

    Create 17265

--- 9465.cpp
+++ 9465.cpp
@@ -4,26 +4,11 @@
 
 using namespace std;
 
-int T, n;
-int sticker[2][100000] {};
-
-void dp(void)
+int main(void)
 {
-    int memo[2] = {sticker[0][0], sticker[1][0]};
-    int tmp[2];
+    int T, n;
+    int sticker[2][100000];
 
-    for (int i = 1; i < n; i++) {
-        tmp[0] = memo[0]; tmp[1] = memo[1];
-        for (int j = 0; j < 2; j++) {
-            memo[j] = MAX(tmp[j], sticker[j][i] + tmp[!j]);
-        }
-    }
-
-    cout << MAX(memo[0], memo[1]) << endl;
-}
-
-void solve(void)
-{
     cin >> T;
     for (int i = 0; i < T; i++) {
         cin >> n;
@@ -32,13 +17,18 @@ void solve(void)
                 cin >> sticker[j][k];
             }
         }
-        dp();
-    }
-}
 
-int main(void)
-{
-    solve();
+        int memo[2] = {sticker[0][0], sticker[1][0]};
+        int tmp[2];
+
+        for (int i = 1; i < n; i++) {
+            tmp[0] = memo[0]; tmp[1] = memo[1];
+            for (int j = 0; j < 2; j++) {
+                memo[j] = MAX(tmp[j], sticker[j][i] + tmp[!j]);
+            }
+        }
+        cout << MAX(memo[0], memo[1]) << endl;
+    }
 
     return 0;
 }
