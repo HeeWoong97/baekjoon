ad0b6dbe2b9b1df6957f12786a5ab046ad516292
HeeWoong97 <ungung97@DESKTOP-HG6HCNO.localdomain>
2021-01-28 17:08:59 +0900

    Modify 3107

--- 3107.cpp
+++ 3107.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-string IPv6;
-string result;
+char IPv6[40];
+char result[40];
 int cnt;
-int chunk;
+int diff;
 
-void insert(int idx)
+void insert(int idx, int num)
 {
-    for (int i = 0; i < strlen(IPv6) - idx; i++) {
-        IPv6[strlen(IPv6) - i] = IPv6[strlen(IPv6) - i - 1];
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < strlen(result) - idx; j++) {
+            result[strlen(result) - j] = result[strlen(result) - j - 1];
+        }
+        result[idx + i] = '0';
     }
-    IPv6[idx] = '0';
 }
 
 void solve(void)
 {
-    int cur;
-
-    for (int i = 0; i < IPv6.length(); i++) {
-        if (IPv6[i] == ':' && i % 5 != 0) {
-            
+    for (int i = 0; i < strlen(IPv6); i++) {
+        if (IPv6[i] == ':') {
+            chunk++;
         }
     }
+    chunk++;
+
+    for (int i = 0; i < strlen(IPv6); i++) {
+        if (IPv6[i + 1] == '\0') {
+            result[i + diff] = IPv6[i];
+            insert(i + diff - cnt, 4 - cnt - 1);
+            break;
+        }
+        if (IPv6[i] == ':') {
+            result[i + diff] = IPv6[i];
+            insert(i + diff - cnt, 4 - cnt);
+            diff += 4 - cnt;
+            cnt = 0;
+            continue;
+        }
+        result[i + diff] = IPv6[i];
+        result[i + diff + 1] = '\0';
+        cnt++;
+    }
+    cout << result << endl;
 }
 
 void input(void)
